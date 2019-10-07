c3bc6691f43ad8d43deaa8d3680759d764ade9b1
alok <alokuttam.shukla@gmail.com>
2019-10-08 05:57:04 +0700

    Udacity with python3 solution

---
+++ Stock_Perdictor/Test.py
@@ -0,0 +1,14 @@
+#https://classroom.udacity.com/courses/ud120/lessons/2254358555/concepts/24196685390923
+#Solution for this
+from sklearn.naive_bayes import GaussianNB
+def classify(features_train, labels_train):   
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+    
+    
+    ### your code goes here!
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    return clf
